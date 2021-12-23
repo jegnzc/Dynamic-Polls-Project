e4d97c795ccc1ac2e3a28ed705947b2879ede2e2
Jorge González <jegnzc@gmail.com>
2021-12-22 21:35:22 -0600

    Initial project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevelTest.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevelTest.Startup))]
+namespace DevelTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
